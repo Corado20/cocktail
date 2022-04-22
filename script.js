95c894a81ad4d02b40cdcbbfed70b11fba8406e3
Corado20 <corado.andrei92@gmail.com>
2022-04-22 16:53:30 +0300

    cocktail project

---
+++ script.js
@@ -0,0 +1,30 @@
+let poza, titlu;
+let selectBtn = document.getElementById("btn");
+document.addEventListener("DOMContentLoaded", onClick);
+selectBtn.addEventListener("click", onClick);
+function onClick() {
+  poza = document.getElementById("poza");
+  titlu = document.getElementById("denumire");
+  getRandomCocktail();
+}
+async function getRandomCocktail() {
+  let res = await fetch(
+    "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+  );
+  let json = await res.json();
+  let data = json.drinks[0];
+  titlu.textContent = data.strDrink;
+  poza.src = `${data.strDrinkThumb}/preview`;
+  const list = document.getElementById("lista");
+  list.innerHTML = "";
+  for (let i = 1; i < 16; i++) {
+    let ingredient = data[`strIngredient${i}`];
+    let cantitate = data[`strMeasure${i}`];
+    if (ingredient) {
+      let li = document.createElement("li");
+      li.textContent = ingredient + (cantitate ? ` - ${cantitate}` : "");
+      list.appendChild(li);
+    } else break;
+  }
+  document.getElementById("indicatii").textContent = data.strInstructions;
+}
